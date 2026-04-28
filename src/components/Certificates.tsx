182be81cddbaa833e81928ad39e2063379e165f7
Hugolelis <hugodelelis05@gmail.com>
2026-04-28 19:38:39 -0300

    (fixed): trocando iframe por embed

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -64,7 +64,7 @@ export function Certificates() {
                 </button>
               </div>
             </div>
-            <iframe
+            <embed
               src={selected.file}
               className={styles.viewer}
               title={selected.name}
